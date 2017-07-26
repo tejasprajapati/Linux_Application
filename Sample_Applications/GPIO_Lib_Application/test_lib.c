e937721054d6d69a795b1b5fd5a36ecfdaa874c0
Tejas Prajapati <prajapatitejas91@gmail.com>
2017-07-26 22:49:59 +0530

    SampleApplication: gpio lib sample application added.
    Signed-off-by: Tejas Prajapati <prajapatitejas91@gmail.com>

---
+++ Sample_Applications/GPIO_Lib_Application/test_lib.c
@@ -0,0 +1,27 @@
+/*
+ * test_lib.c
+ *
+ *  Created on: Jul 26, 2017
+ *      Author: Tejas Laptop
+ */
+
+#include <stdio.h>
+#include <gpio.h>
+
+int main()
+{
+	int i;
+	GPIOExport(4);																/*	Enable GPIO pin for use*/
+	sleep(1);
+	GPIODirection(4,1);															/*	Set direction for GPIO*/
+
+	for(i = 0; i < 10; i++)
+		{
+			printf("Writing gpio\n");
+			if (GPIOWrite(4, i % 2))											/*	Write GPIO value*/
+			usleep(500 * 1000);
+		}
+
+	GPIOUnexport(4);															/*	Disable GPIO pin after use*/
+	return 0;
+}
