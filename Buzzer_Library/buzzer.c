00125dac6b3d34381f8c3aebcf12872571f2c8fe
Tejas Prajapati <prajapatitejas91@gmail.com>
2017-07-27 20:39:57 +0530

    buzzerlib: Added a library for buzzer.
    Signed-off-by: Tejas Prajapati <prajapatitejas91@gmail.com>

---
+++ Buzzer_Library/buzzer.c
@@ -0,0 +1,68 @@
+/*
+ * buzzer.c
+ *
+ *  Created on: Jul 27, 2017
+ *      Author: Tejas Laptop
+ */
+
+#include <gpio.h>
+#include "buzzer.h"
+
+int Buzzer_Init(int Pin)
+{
+	if(GPIOExport(Pin) == 0)													/*	Enable GPIO pin for use*/
+	{
+		sleep(1);
+		if(GPIODirection(Pin,OUT))												/*	Set direction for GPIO*/
+			goto ERROR_INIT;
+	}
+	else
+		goto ERROR_INIT;
+
+	return 0;
+
+	ERROR_INIT:
+	printf("Failed to open the %d GPIO for buzzer\n",Pin);
+	return -1;
+}
+
+
+
+int Buzzer_On(int Pin)
+{
+	if(GPIOWrite(Pin, 1))														/*	Write GPIO value*/
+	{
+		printf("Error writing to GPIO pin %d\n",Pin);
+		return -1;
+	}
+	return 0;
+}
+
+int Buzzer_Off(int Pin)
+{
+	if(GPIOWrite(Pin, 0))														/*	Write GPIO value*/
+	{
+		printf("Error writing to GPIO pin %d\n",Pin);
+		return -1;
+	}
+	return 0;
+}
+
+int Buzzer_Toggle(int Pin, int Delay)
+{
+	if(GPIOWrite(Pin,!GPIORead(Pin)))														/*	Write GPIO value*/
+	{
+		printf("Error writing to GPIO pin %d\n",Pin);
+		return -1;
+	}
+
+	usleep(Delay);
+
+	if(GPIOWrite(Pin,!GPIORead(Pin)))														/*	Write GPIO value*/
+	{
+		printf("Error writing to GPIO pin %d\n",Pin);
+		return -1;
+	}
+
+	return 0;
+}
---
+++ Buzzer_Library/buzzer.h
@@ -0,0 +1,19 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Jul 27, 2017
+ *      Author: Tejas Laptop
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+int Buzzer_Init(int);
+
+int Buzzer_On(int);
+
+int Buzzer_Off(int);
+
+int Buzzer_Toggle(int,int);
+
+#endif /* BUZZER_H_ */
